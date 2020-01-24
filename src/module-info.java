fccfb1cee6222a369ab96f7929824c4b5e247b7f
artur <artur@WarriorGaminMachine>
2020-01-23 19:18:53 -0600

    Corriendo con 13

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module LogicProcessor {
+	exports com.adhoc.java.processor.logic.interfaces;
+	exports com.adhoc.java.processor.logic.utils;
+	exports com.adhoc.java.processor.logic.operators;
+	exports com.adhoc.java.processor.ui;
+	exports com.adhoc.java.processor.logic;
+
+	requires java.desktop;
+	requires java.logging;
+}
